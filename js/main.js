21dee30617b0d00f4c8c658aafa30d88bb23b847
juvobe <juusovb@gmail.com>
2025-07-30 23:35:06 +0300

    naming

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-//Mobile Menu
+//Mobile menu
 document.addEventListener('DOMContentLoaded', () => {
   const hamburgerButton = document.querySelector('.hamburger-button');
   const mobileMenu = document.querySelector('.mobile-menu');
